fabc5dfa6cd70e1dfd83bb2b75dd56f69354b39c
노우현 <rkjnice@gmail.com>
2017-03-11 13:45:02 +0900

    fix for filter += ResultSet

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -596,7 +596,6 @@ class OGame(object):
         eventsDup = filter(lambda x: 'partnerInfo' not in x.get('class', []), events)
         events = soup.findAll('tr', {'class': 'allianceAttack'})
         events += eventsDup
-        # unsupported operand type(s) for +=: 'filter' and 'ResultSet'
         attacks = []
         for event in events:
             mission_type = int(event['data-mission-type'])
