a775218f3369d4591c2a57ad7b5b547fa00b647e
rladlb <romain.lima@gmail.com>
2017-03-31 16:16:20 +0200

    Ajout des formules + fonction de calcule d'energe

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -123,6 +123,12 @@ def get_code(name):
     print('Couldn\'t find code for %s' % name)
     return None
 
+def building_cost(lvl, type):
+    cost = {}
+    cost['metal'] = int(math.floor(constants.Formules[type]['Metal'][0]*constants.Formules[type]['Metal'][1]**(lvl-1)))
+    cost['crystal'] = int(math.floor(constants.Formules[type]['Crystal'][0]*constants.Formules[type]['Crystal'][1]**(lvl-1)))
+    cost['deuterium'] = int(math.floor(constants.Formules[type]['Deuterium'][0]*constants.Formules[type]['Deuterium'][1]**(lvl-1)))
+    return cost
 
 @for_all_methods(sandbox_decorator)
 class OGame(object):
@@ -956,37 +962,25 @@ class OGame(object):
     def galaxy_infos(self, galaxy, system):
         html = self.galaxy_content(galaxy, system)['galaxy']
         soup = BeautifulSoup(html, 'lxml')
-        rows = soup.findAll('tr', {'class': 'row'})
-        res = []
-        for row in rows:
-            if 'empty_filter' not in row.get('class'):
-                tooltips = row.findAll('div', {'class': 'htmlTooltip'})
-                planet_tooltip = tooltips[0]
-                planet_name = planet_tooltip.find('h1').find('span').text
-                planet_url = planet_tooltip.find('img').get('src')
-                coords_raw = planet_tooltip.find('span', {'id': 'pos-planet'}).text
-                coords = re.search(r'\[(\d+):(\d+):(\d+)\]', coords_raw)
-                galaxy, system, position = coords.groups()
-                planet_infos = {}
-                planet_infos['name'] = planet_name
-                planet_infos['img'] = planet_url
-                planet_infos['coordinate'] = {}
-                planet_infos['coordinate']['galaxy'] = int(galaxy)
-                planet_infos['coordinate']['system'] = int(system)
-                planet_infos['coordinate']['position'] = int(position)
-                if len(tooltips) > 1:
-                    player_tooltip = tooltips[1]
-                    player_id_raw = player_tooltip.get('id')
-                    player_id = int(re.search(r'player(\d+)', player_id_raw).groups()[0])
-                    player_name = player_tooltip.find('h1').find('span').text
-                    player_rank = parse_int(player_tooltip.find('li', {'class': 'rank'}).find('a').text)
-                else:
-                    player_id = None
-                    player_name = row.find('td', {'class': 'playername'}).find('span').text.strip()
-                    player_rank = None
-                planet_infos['player'] = {}
-                planet_infos['player']['id'] = player_id
-                planet_infos['player']['name'] = player_name
-                planet_infos['player']['rank'] = player_rank
-                res.append(planet_infos)
-        return res
+        form = soup.find('form', {'id': 'planetMaintenanceDelete'})
+        abandon = form.find('input', {'name': 'abandon'}).get('value')
+        token = form.find('input', {'name': 'token'}).get('value')
+        payload = {'abandon': abandon,
+                  'token': token,
+                  'password': self.password} 
+        headers = {'X-Requested-With': 'XMLHttpRequest'}
+        check_password = self.session.post(self.get_url('checkPassword'), headers=headers, data=payload).content
+        jo = json.loads(check_password)
+        new_token = jo['newToken']
+        delete_payload = {'abandon': abandon,
+                         'token': new_token,
+                         'password': self.password}
+        
+        delete_action = self.session.post(self.get_url('planetGiveup'), headers=headers, data=delete_payload).content   
+
+def Consommation(batiment, lvl):
+    """ Retourne la consommation du batiment du level lvl + 1 """
+    energieLvl = Formules['batiments'][batiment]['consommation'][0] * lvl * (Formules['batiments'][batiment]['consommation'][1]**lvl)
+    energieNextLvl = Formules['batiments'][batiment]['consommation'][0] * (lvl+1) * (Formules['batiments'][batiment]['consommation'][1]**(lvl+1))
+    return math.floor(energieNextLvl - energieLvl)
+
