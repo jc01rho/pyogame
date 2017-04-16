a83f2a505606ffeecd3aac39dd76c265ea61e129
Giancarlo Ventura <sakya_stelios@hotmail.com>
2017-04-16 18:14:51 -0500

    Update __init__.py

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -813,3 +813,9 @@ class OGame(object):
             return '0'
         return fleets.contents[0]
     
+    def jumpgate_execute(self):
+        res = self.session.get(self.get_url('jumpgate_execute')).content
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        return True
+    
+    
