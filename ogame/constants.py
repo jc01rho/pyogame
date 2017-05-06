d5a2fb44bbf4283fea67e32a71b9a449fd02746d
노우현 <rkjnice@gmail.com>
2017-05-07 02:44:00 +0900

    fix for english

--- ogame/constants.py
+++ ogame/constants.py
@@ -167,7 +167,7 @@ Missions = {'Attack': 1,
 
 
 Formules = {
-        'batiments' : {
+        'Buildings' : {
             'metal_mine': { 
                 'cout': {
                     'Metal':[60,1.5], 'Crystal':[15,1.5], 'Deuterium':[0,0]
@@ -190,7 +190,7 @@ Formules = {
                 'consommation': [20,1.1]
             },
         },
-        'energy' : {
+        'Energy' : {
             'solar_plant':{
                 'cout': {
                     'Metal':[75,1.5], 'Crystal':[30,1.5], 'Deuterium':[0,0]
@@ -213,7 +213,7 @@ Formules = {
                 'consommation': [10,1.1]
             },
         },
-        'storage' :{
+        'Storage' :{
             'metal_storage':{
                 'cout': {
                     'Metal':[0,0], 'Crystal':[0,0], 'Deuterieum':[0,0]
