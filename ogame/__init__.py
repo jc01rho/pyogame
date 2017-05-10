254cafc42943c14a771e46ddb1a7a4aa18197887
노우현 <rkjnice@gmail.com>
2017-05-11 00:32:00 +0900

    fix constants for ress_storage

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -1053,3 +1053,8 @@ class OGame(object):
         return cost
 
 
+    def storageSize(self, type, batiment, lvl):
+        capacity = -1
+        capacity = 5000 * int(math.floor(2.5 * (math.e ** ( constants.Formules[type][batiment] * 20 / 33))))
+        return capacity
+
