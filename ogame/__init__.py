37ffad8684194e4511cd60815c7c964a252b111d
노우현 <whrho@fasoo.com>
2017-05-11 19:07:30 +0900

    update for under construction check

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -4,14 +4,16 @@ import math
 import re
 import time
 import arrow
-import requests
-import json
+import requests, requests.utils
+import pickle
 
 from ogame import constants
 from ogame.errors import BAD_UNIVERSE_NAME, BAD_DEFENSE_ID, NOT_LOGGED, BAD_CREDENTIALS, CANT_PROCESS, BAD_BUILDING_ID, \
     BAD_SHIP_ID, BAD_RESEARCH_ID
 from bs4 import BeautifulSoup
 from dateutil import tz
+
+miniFleetToken = None
 
 proxies = {
     'http': 'socks5://127.0.0.1:9050',
@@ -127,7 +129,7 @@ class OGame(object):
                  sandbox=False, sandbox_obj=None, use_proxy=False):
         self.session = requests.session()
         self.session.headers.update({
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'})
+                                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'})
         self.sandbox = sandbox
         self.sandbox_obj = sandbox_obj if sandbox_obj is not None else {}
         self.universe = universe
@@ -530,8 +532,8 @@ class OGame(object):
         payload.update({'crystal': resources.get('crystal'),
                         'deuterium': resources.get('deuterium'),
                         'metal': resources.get('metal'),
-                        'mission': mission ,
-                        'holdingtime' : acsDefHoldTime
+                        'mission': mission,
+                        'holdingtime': acsDefHoldTime
                         })
         res = self.session.post(self.get_url('movement'), data=payload).content
 
@@ -585,9 +587,9 @@ class OGame(object):
         for event in events:
             mission_type = int(event['data-mission-type'])
             if mission_type not in [1, 2]:
-                if checkSpyAlso and mission_type not in [6] :
+                if checkSpyAlso and mission_type not in [6]:
                     continue
-                elif checkSpyAlso is False :
+                elif checkSpyAlso is False:
                     continue
                 else:
                     None
@@ -641,8 +643,9 @@ class OGame(object):
             second = int(second)
             arrival_time = self.get_datetime_from_time(hour, minute, second)
             attack.update({'arrival_time': arrival_time})
-            #todo Mn replace 제대로
-            attack.update({'detailsFleet': int(event.find('td', {'class': 'detailsFleet'}).text.replace(".","").replace("Mn","").strip())})
+            # todo Mn replace 제대로
+            attack.update({'detailsFleet': int(
+                event.find('td', {'class': 'detailsFleet'}).text.replace(".", "").replace("Mn", "").strip())})
 
             if mission_type == 1:
                 attacker_id = event.find('a', {'class': 'sendMail'})['data-playerid']
@@ -728,6 +731,12 @@ class OGame(object):
 
         infos_label = BeautifulSoup(link['title'], 'lxml').text
         infos = get_planet_infos_regex(infos_label)
+        isUnderConstruction = soup.find('table', {'class': "construction active"}).find('td', {'class': "desc timer"})
+        if isUnderConstruction is None:
+            isUnderConstruction = False
+        else:
+            isUnderConstruction = True
+
         res = {}
         res['img'] = link.find('img').get('src')
         res['id'] = planet_id
@@ -741,6 +750,7 @@ class OGame(object):
         res['fields']['built'] = int(infos.group(6))
         res['fields']['total'] = int(infos.group(7))
         res['temperature'] = {}
+        res['isUnderConstruction'] = isUnderConstruction
         if infos.groups().__len__() > 7:  # is a planet
             res['temperature']['min'] = int(infos.group(8))
             res['temperature']['max'] = int(infos.group(9))
@@ -829,7 +839,6 @@ class OGame(object):
             raise NOT_LOGGED
         return obj
 
-
     def get_spy_reports(self):
         headers = {'X-Requested-With': 'XMLHttpRequest'}
         payload = {'tab': 20,
@@ -876,7 +885,6 @@ class OGame(object):
         res = self.session.post(url, data=payload, headers=headers).content.decode('utf8')
         return res
 
-
     def get_flying_fleets(self):
         url = self.get_url('movement')
         res = self.session.get(url).content
@@ -886,7 +894,7 @@ class OGame(object):
         if fleets is None:
             return '0'
         return fleets.contents[0]
-    
+
     def jumpgate_execute(self):
         res = self.session.get(self.get_url('jumpgate_execute')).content
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -894,19 +902,24 @@ class OGame(object):
 
     def Consommation(self, type, batiment, lvl):
         """ Retourne la consommation du batiment du level lvl + 1 """
-        energieLvl = constants.Formules[type][batiment]['consommation'][0] * lvl * (constants.Formules[type][batiment]['consommation'][1]**lvl)
-        energieNextLvl = constants.Formules[type][batiment]['consommation'][0] * (lvl+1) * (constants.Formules[type][batiment]['consommation'][1]**(lvl+1))
+        energieLvl = constants.Formules[type][batiment]['consommation'][0] * lvl * (
+        constants.Formules[type][batiment]['consommation'][1] ** lvl)
+        energieNextLvl = constants.Formules[type][batiment]['consommation'][0] * (lvl + 1) * (
+        constants.Formules[type][batiment]['consommation'][1] ** (lvl + 1))
         return math.floor(energieNextLvl - energieLvl)
 
     def building_cost(self, type, batiment, lvl):
         """ Retourne le cout d'un batiment lvl + 1 """
         cost = {}
-        cost['metal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Metal'][0]*constants.Formules[type][batiment]['cout']['Metal'][1]**(lvl-1)))
-        cost['crystal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Crystal'][0]*constants.Formules[type][batiment]['cout']['Crystal'][1]**(lvl-1)))
-        cost['deuterium'] = int(math.floor(constants.Formules[type][batiment]['cout']['Deuterium'][0]*constants.Formules[type][batiment]['cout']['Deuterium'][1]**(lvl-1)))
+        cost['metal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Metal'][0] *
+                                       constants.Formules[type][batiment]['cout']['Metal'][1] ** (lvl - 1)))
+        cost['crystal'] = int(math.floor(constants.Formules[type][batiment]['cout']['Crystal'][0] *
+                                         constants.Formules[type][batiment]['cout']['Crystal'][1] ** (lvl - 1)))
+        cost['deuterium'] = int(math.floor(constants.Formules[type][batiment]['cout']['Deuterium'][0] *
+                                           constants.Formules[type][batiment]['cout']['Deuterium'][1] ** (lvl - 1)))
         return cost
 
     def storageSize(self, type, batiment, lvl):
         capacity = -1
-        capacity = 5000 * int(math.floor(2.5 * (math.e ** ( constants.Formules[type][batiment] * 20 / 33))))
+        capacity = 5000 * int(math.floor(2.5 * (math.e ** (constants.Formules[type][batiment] * 20 / 33))))
         return capacity
