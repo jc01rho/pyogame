4caf9ed120c8b727d191631db7a76de75164e831
The Pequichi <ForPequi@users.noreply.github.com>
2017-07-13 16:47:55 -0500

    Update __init__.py

--- ogame/__init__.py
+++ ogame/__init__.py
@@ -983,7 +983,6 @@ class OGame(object):
         form = soup.find('form', {'id': 'planetMaintenanceDelete'})
         abandon = form.find('input', {'name': 'abandon'}).get('value')
         token = form.find('input', {'name': 'token'}).get('value')
-        token = 
         payload = {'abandon': abandon,
                   'token': token
                   'password': self.password}        
